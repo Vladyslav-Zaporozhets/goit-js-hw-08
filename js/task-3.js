4fdd0d7f4f6a8d391e01a874761bbdae7c118499
Vladyslav Zaporozhets <ziper.step@gmail.com>
2025-08-31 15:41:10 +0200

    add

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const nameInput = document.querySelector('#name-input');
-const nameOutput = document.querySelector('#name-output');
-
-nameInput.addEventListener('input', (event) => {
-  const trimmedValue = event.currentTarget.value.trim();
-
-  nameOutput.textContent = trimmedValue || 'Anonymous';
-});
